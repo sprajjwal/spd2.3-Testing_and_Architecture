21b20d996cadba2e960cef9fe61bc02111bd6e17
sprajjwal <sprajjwal@ucdavis.edu>
2021-02-12 12:06:13 -0800

    part 3 refactoring done

---
+++ lab/refactoring/decompose_conditional.py
@@ -0,0 +1,32 @@
+
+# By Kami Bigdely
+# Decompose conditional: You have a complicated conditional(if-then-else) statement. Extract
+# methods from the condition, then part, and else part(s).
+
+def make_alert_sound():
+    print('made alert sound.')
+
+
+def make_accept_sound():
+    print('made acceptance sound')
+
+
+def is_toxic(ingredients):
+    return (
+        'sodium nitrate' in ingredients 
+        or 'sodium benzoate' in ingredients
+        or 'sodium oxide' in ingredients
+    )
+
+
+ingredients = ['sodium benzoate']
+if is_toxic(ingredients):
+    print('!!!')
+    print('there is a toxin in the food!')    
+    print('!!!')
+    make_alert_sound()
+else:
+    print('***')
+    print('Toxin Free')
+    print('***')
+    make_accept_sound()

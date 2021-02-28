5ec9c7679e266feb658ca4e11f246d8278b74001
sprajjwal <sprajjwal@ucdavis.edu>
2021-02-27 18:06:43 -0800

    test updates

--- pytest-tut/test_exercise_1.py
+++ pytest-tut/test_exercise_1.py
@@ -14,17 +14,15 @@ def get_average(li):
 
 def test_get_average_expected_inputs():
     # all pos
-    assert get_average([2, 2, 2, 2]) == 2
+    assert get_average([1, 3, 5, 7]) == 4
     # pos integers and floats
-    assert get_average([2.5, 2, 2.5, 2]) == 2.25
+    assert get_average([0.5, 2, 2.5, 3.5]) == 2.25
     # pos floats
-    assert get_average([3.33, 9.99]) == 6.66
+    assert get_average([1.2, 1.4]) == 1.3
     # pos and neg ints
-    assert get_average([2, 2, 2, -22]) == -4
+    assert get_average([-1, -2, 1, 2]) == 0
     # pos, neg, ints, and floating points
-    assert get_average([4.78, -7, 2.22, -22]) == -5.5
-    assert math.isclose(get_average([10.1, 0.01, 0.1]), 3.4033333333)
-
+    
 
 def test_get_average_empty_list():
     assert math.isnan(get_average([]))
